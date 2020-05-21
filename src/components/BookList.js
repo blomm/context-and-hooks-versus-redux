6081cd8a05f03ba9cf55b3918afef1f08aead72c
Mike Blom <mikeblom@Mikes-MacBook-Pro.local>
2020-05-21 16:53:48 +0100

    app setup

---
+++ src/components/BookList.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+class BookList extends Component {
+  render() {
+    return (
+      <div className="book-list">
+        <ul>
+          <li>Crime and Punishment</li>
+          <li>Cloudy with a Chance of Meatballs</li>
+          <li>God of Small Things</li>
+        </ul>
+      </div>
+    )
+  }
+}
+
+export default BookList
